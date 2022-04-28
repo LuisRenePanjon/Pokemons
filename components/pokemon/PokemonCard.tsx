5558d21a94e8032c055564b7ed4015e99285a4a0
Luis René Panjón <luchorene97@gmail.com>
2022-04-28 18:06:41 -0500

    Navigate to  selected pokemon page

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,15 +1,24 @@
 import { FC } from 'react';
 import { SmallPokemon } from '../../interfaces';
 import {  Card, Grid, Row, Text } from '@nextui-org/react';
+import { useRouter } from 'next/router';
 
 interface Props {
     pokemon: SmallPokemon;
 }
 
 export const PokemonCard: FC<Props> = ({ pokemon }) => {
+
+    const router = useRouter();
+
+    // change page to pokemon page
+    const handleClick = () => {
+        router.push(`/pokemon/${pokemon.id}`);
+    }
+
     return (
         <Grid xs={6} sm={3} key={pokemon.id}>
-            <Card hoverable clickable>
+            <Card hoverable clickable onClick={handleClick}>
                 <Card.Body css={{ p: 0 }}>
                     <Card.Image
                         objectFit='contain'
