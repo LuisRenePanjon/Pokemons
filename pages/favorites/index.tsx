69cd027c015d7bd4a15ce0f8a7bcd7792f264305
Luis René Panjón <luchorene97@gmail.com>
2022-05-03 12:02:13 -0500

    Add favorite page

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,11 @@
+import { MainLayout } from "../../components/layouts";
+
+const FavoritesPage = () => {
+    return (
+        <MainLayout title="Favoritos">
+            <h1>Favorities</h1>
+        </MainLayout>
+    );
+};
+
+export default FavoritesPage;
