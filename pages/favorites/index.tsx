523b3e2744ee3ed2b55080d20c2665a6dc7b6003
Luis René Panjón <luchorene97@gmail.com>
2022-05-06 17:52:44 -0500

    Create Favorte Page

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,9 +1,23 @@
-import { MainLayout } from "../../components/layouts";
+import { useState, useEffect } from 'react';
+import { MainLayout } from '../../components/layouts';
+import { NoFavorites } from '../../components/ui';
+import { localStFavorites } from '../../utils';
+import { FavoritePokemons } from '../../components/pokemon';
 
 const FavoritesPage = () => {
+    const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+
+    useEffect(() => {
+        setFavoritePokemons(localStFavorites.getFavoritePokemons());
+    }, []);
+
     return (
-        <MainLayout title="Favoritos">
-            <h1>Favorities</h1>
+        <MainLayout title='Favoritos'>
+            {favoritePokemons.length > 0 ? (
+                <FavoritePokemons pokemons={favoritePokemons}/>
+            ) : (
+                <NoFavorites />
+            )}
         </MainLayout>
     );
 };
