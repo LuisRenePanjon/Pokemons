f4a2b7d3bc1171c5e6308a49ad8aa57d227ea729
Rene Panjon windows <luchorene97@gmailcom>
2022-05-07 16:42:54 -0500

    Add validation if pokemon exist

--- adapters/getBasicPokemon.ts
+++ adapters/getBasicPokemon.ts
@@ -2,13 +2,20 @@ import { pokeApi } from '../api';
 import { PokemonResponse } from '../interfaces';
 
 export const getBasicPokemon = async (parameter: string) => {
-    const { data } = await pokeApi.get<PokemonResponse>(
-        `/pokemon/${parameter}`
-    );
-    return {
-        id: data.id,
-        name: data.name,
-        sprites: data.sprites,
-        abilities: data.abilities,
-    };
+
+    try {
+        const { data } = await pokeApi.get<PokemonResponse>(
+            `/pokemon/${parameter}`
+        );
+        return {
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites,
+            abilities: data.abilities,
+        };
+        
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
 };
